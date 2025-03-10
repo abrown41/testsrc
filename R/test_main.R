c7a65ed6393f4c42cce63602404d7e971875b0ef
Andrew Brown <andrew.brown@qub.ac.uk>
2025-03-10 12:53:30 +0000

    fix AutoFeedbackR dependency

--- R/test_main.R
+++ R/test_main.R
@@ -3,8 +3,8 @@
 #' @return Invisible TRUE/FALSE indicating test success
 #' @export
 test_x <- function() {
-  # Import check_var from studentfeedback (already handled by NAMESPACE)
-  studentfeedback::check_var("x", 42)
+  # Import check_var from AutoFeedbackR (already handled by NAMESPACE)
+  AutoFeedbackR::check_var("x", 42)
 }
 
 #' Test if variable y is correctly defined
@@ -13,7 +13,7 @@ test_x <- function() {
 #' @export
 test_y <- function() {
   # Test that y is a vector of specified values
-  studentfeedback::check_var("y", c(1, 2, 3, 4, 5))
+  AutoFeedbackR::check_var("y", c(1, 2, 3, 4, 5))
 }
 
 #' Test if variable z is correctly defined
@@ -26,5 +26,5 @@ test_z <- function() {
     a = c(1, 2, 3),
     b = c("x", "y", "z")
   )
-  studentfeedback::check_var("z", expected_z)
+  AutoFeedbackR::check_var("z", expected_z)
 }
